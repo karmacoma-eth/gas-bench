f3e388cd6ea9fbdecb8f3a4319a913c0aef4782f
karmacoma <karma@coma.lol>
2023-03-06 15:55:43 -0800

    Add multiple mints to BasicERC721.t.sol

--- test/BasicERC721.t.sol
+++ test/BasicERC721.t.sol
@@ -19,9 +19,31 @@ contract BasicERC721Test is Test {
         nftContract.mint();
     }
 
-    function test_mint_newcomer() public {
+    function mint(uint256 n) private {
+        unchecked {
+            for (uint160 i = 0; i < n; i++) {
+                address recipient = address(uint160(bob) + i);
+                vm.prank(recipient);
+                nftContract.mint();
+            }
+        }
+    }
+
+    function test_mint_newcomer_0001() public {
         vm.prank(bob);
         nftContract.mint();
+    }
+
+    function test_mint_newcomer_0010() public {
+        mint(10);
+    }
+
+    function test_mint_newcomer_0100() public {
+        mint(100);
+    }
+
+    function test_mint_newcomer_1000() public {
+        mint(1000);
     }
 
     function test_mint_existing() public {
